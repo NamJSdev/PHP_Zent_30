79f67c34951c80ce4602955f82dba3c7353588a6
namjsdev <nam.js.dev@gmail.com>
2022-03-21 15:15:34 +0700

    Unit03

---
+++ Unit03/Ex1.php
@@ -0,0 +1,9 @@
+<?php
+    $array_name = array('NamJS',50,'Nguyen hai nam');
+    // echo "<br> Ten toi la: ".$array_name[0];
+    $arr_info = array();
+    $arr_info['MSV'] = 93940384039;
+    foreach($array_name as $key => $value){
+        echo $key." : ".$value."<br>";
+    }
+?>  
