655c41037dfc361a95a6d3bb8b91780439e7d346
namjsdev <nam.js.dev@gmail.com>
2022-03-22 22:26:28 +0700

    updateU5

---
+++ Unit03/Ex5/index.php
@@ -0,0 +1,8 @@
+<?php
+    include_once('layoutTop.php');
+    include_once('header.php');
+    include_once('sidebar.php');
+    echo "Main";
+    require_once('footer.php');
+    include_once('layoutBottom.php');
+?>
---
+++ Unit03/Ex5/user.php
@@ -0,0 +1,8 @@
+<?php
+    include_once('layoutTop.php');
+    include_once('header.php');
+    include_once('sidebar.php');
+    echo "User";
+    require_once('footer.php');
+    include_once('layoutBottom.php');
+?>
